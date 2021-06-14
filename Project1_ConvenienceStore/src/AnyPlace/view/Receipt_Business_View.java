9990bd1e1d150f167b7c88999f85bbea1a9d68eb
austh <austh@DESKTOP-J0LR7JR>
2021-06-14 13:59:09 +0900

    Join 복구

---
+++ Project1_ConvenienceStore/src/AnyPlace/view/Receipt_Business_View.java
@@ -0,0 +1,17 @@
+package AnyPlace.view;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JFrame;
+
+public class Receipt_Business_View extends JFrame{
+	public Receipt_Business_View() {
+		setTitle("Receipt_Business_View");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.add(pane, BorderLayout.CENTER);
+		setSize
+	}
+
+}
