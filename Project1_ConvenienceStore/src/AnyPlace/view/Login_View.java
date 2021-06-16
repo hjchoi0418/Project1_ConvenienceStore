8bb18b1c0d4e454c5194dfb23f39d368f2d96bfc
WonHyeYoung <demian_holloway@naver.com>
2021-06-16 13:24:05 +0900

    로그인 수정

--- Project1_ConvenienceStore/src/AnyPlace/view/Login_View.java
+++ Project1_ConvenienceStore/src/AnyPlace/view/Login_View.java
@@ -6,106 +6,61 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
-import java.util.ArrayList;
-
-import javax.print.DocFlavor.URL;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
-import java.awt.event.ActionEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+import AnyPlace.controller.Login;
 
-public class Login_View extends JFrame {
+
+public class Login_View extends JFrame implements ActionListener{
+	
+	Login login=new Login();
 	ImageIcon icon;
 	JTextField id_field = new JTextField();
 	private JTextField textField;
-	
-		
 
 	public Dimension getPreferredSize(){
         Dimension largeBtnSz = new Dimension(super.getPreferredSize().width+30, super.getPreferredSize().height+30);
         return largeBtnSz;
-    }
+	}
 
-	     public Login_View() {
-	    	
-	    	 setTitle("Any Place");
-	    	 
-	    	 getContentPane().setLayout(null);
-	    	 
-//icon = new ImageIcon("./img/로그인_배경.jpg");
-//	    	 
-//	    	 JPanel background = new JPanel() {
-//	             public void paintComponent(Graphics g) {
-//	                 // Approach 1: Dispaly image at at full size
-//	                 g.drawImage(icon.getImage(), 0, 0, null);
-//	                 // Approach 2: Scale image to size of component
-//	                 // Dimension d = getSize();
-//	                 // g.drawImage(icon.getImage(), 0, 0, d.width, d.height, null);
-//	                 // Approach 3: Fix the image position in the scroll pane
-//	                 // Point p = scrollPane.getViewport().getViewPosition();
-//	                 // g.drawImage(icon.getImage(), p.x, p.y, null);
-//	                 setOpaque(false); //그림을 표시하게 설정,투명하게 조절
-//	                 super.paintComponent(g);
-//	             }
-//	    	 };
-	    	 try {	
-	    			final Image backgroundImage = javax.imageio.ImageIO.read(new File("./img/로그인_배경.jpg"));
-	    		    setContentPane(new JPanel(new BorderLayout()) {
-	    		        @Override public void paintComponent(Graphics g) {
-	    		            g.drawImage(backgroundImage, 0, 0, null);
-	    		            setOpaque(false);
-	    	                super.paintComponent(g);
-	    		        }
-	    		    });
-	    			} catch (IOException e) {
-	    			    throw new RuntimeException(e);
-	    			}
-	    	 
+	public Login_View() {
+
+		setTitle("Any Place");
+
+		getContentPane().setLayout(null);
+
+		try {
+			final Image backgroundImage = javax.imageio.ImageIO.read(new File("./img/로그인_배경.jpg"));
+			setContentPane(new JPanel(new BorderLayout()) {
+				@Override
+				public void paintComponent(Graphics g) {
+					g.drawImage(backgroundImage, 0, 0, null);
+					setOpaque(false);
+					super.paintComponent(g);
+				}
+			});
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
 		JFrame frame = new JFrame();
 		
-
-		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setLocation(1300,700);
+		setLocation(200,200);
 		setSize(1467,902);
 		getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("login");
-		btnNewButton.setForeground(Color.WHITE);
-		btnNewButton.setFont(new Font("나눔고딕", Font.BOLD, 15));
-		btnNewButton.setBackground(new Color(255, 204, 51));
-		btnNewButton.setFocusPainted(false);
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton.setBounds(557, 498, 353, 55);
-		getContentPane().add(btnNewButton);
-		
-		
-		JButton joinButton = new JButton("join");
-		//joinButton.setIcon(new ImageIcon("./img/가입버튼.jpg"));
-	//	joinButton.setSelectedIcon(new ImageIcon("./img/가입버튼.jpg"));
-		joinButton.setForeground(new Color(255, 204, 51));
-		joinButton.setFont(new Font("나눔고딕", Font.PLAIN, 12));
-		joinButton.setBorderPainted(false);
-		joinButton.setContentAreaFilled(false);
-		joinButton.setFocusPainted(false);
-	
-		joinButton.setBounds(850, 550, 74, 58);
-		getContentPane().add(joinButton);
-	     
-	    
-	     
 		textField = new JTextField();
 		textField.setToolTipText("ID");
 		textField.setHorizontalAlignment(SwingConstants.LEFT);
@@ -115,11 +70,70 @@ public class Login_View extends JFrame {
 		textField.setColumns(10);
 		setVisible(true);
 		
-	     }
+		JButton btnNewButton = new JButton("login");
+		btnNewButton.setForeground(Color.WHITE);
+		btnNewButton.setFont(new Font("나눔고딕", Font.BOLD, 15));
+		btnNewButton.setBackground(new Color(255, 204, 51));
+		btnNewButton.setFocusPainted(false);
+		btnNewButton.addActionListener(new ActionListener() {
 
-	public static void main(String[] args) {
-		new Login_View();
-		
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+				Login login = new Login();
+				JTextField id_input = textField;
+				String id = textField.getText();
+
+				if (id.equals(Login.getID(id))) {
+					JOptionPane.showMessageDialog(null, "You have logged in successfully");
+					new sale_View();
+					setVisible(false);
+				} else {
+					JOptionPane.showMessageDialog(null, "You failed to log In");
+				}
+
+			}
+
+		});
+		btnNewButton.setBounds(557, 498, 353, 55);
+		getContentPane().add(btnNewButton);
+
+		JButton joinButton = new JButton("join");
+		// joinButton.setIcon(new ImageIcon("./img/가입버튼.jpg"));
+		// joinButton.setSelectedIcon(new ImageIcon("./img/가입버튼.jpg"));
+		joinButton.setForeground(new Color(255, 204, 51));
+		joinButton.setFont(new Font("나눔고딕", Font.PLAIN, 12));
+		joinButton.setBorderPainted(false);
+//		joinButton.setContentAreaFilled(false);
+		joinButton.setFocusPainted(false);
+
+		joinButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new join_View2();
+				setVisible(false);
+			}
+		});
+
+		joinButton.setBounds(850, 550, 74, 58);
+		getContentPane().add(joinButton);
+
+		//
+		joinButton.addActionListener(this);
+		btnNewButton.addActionListener(this);
+
 	}
-}
 
+	public static void main(String[] args) throws Exception {
+		new Login_View();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
